8e147a8f8b6ce865186e2a511093cb823b7fc4c7
Hyperion <gabriel.violet.dream@gmail.com>
2014-02-03 07:52:36 +0200

    Переписан debug метод для поддержки стилистики

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def debug(object)
+    html = ''
+    if Rails.env == 'development'
+      html << content_tag(:h3, I18n.t(:debug_info), {'data-toggle' => 'collapse', 'data-target' => '#debug_dump', 'style' => 'cursor: pointer'})
+      Marshal::dump(object)
+      object = ERB::Util.html_escape(object.to_yaml).gsub('  ', "&nbsp; ").html_safe
+      html << content_tag(:pre, object, id: 'debug_dump', class: 'pre-scrollable collapse in debug_dump')
+    end
+    html.html_safe
+  rescue Exception
+    content_tag(:code, object.inspect, id: 'debug_dump', class: 'pre-scrollable collapse in debug_dump')
+  end
 end
